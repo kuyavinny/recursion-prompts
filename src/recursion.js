f2dc0ec8126ebe33226d93770fc69752353463cd
Glenmore Vinoya <glenmore@pinoyak.com>
2020-12-30 13:02:10 -0900

    completed sum of nested arrays

--- src/recursion.js
+++ src/recursion.js
@@ -35,6 +35,18 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  let result = 0;
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      result += arraySum(element);
+    } else {
+      result += element;
+    }
+  });
+  return result;
 };
 
 // 4. Check if a number is even.
