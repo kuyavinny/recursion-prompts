3a4d35782fa509eca81969a3c27765e03e947982
Glenmore Vinoya <glenmore@pinoyak.com>
2020-12-30 13:00:38 -0900

    completed sum of an array of integers

--- src/recursion.js
+++ src/recursion.js
@@ -19,6 +19,17 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  if (array.length === 1) {
+    return array[0];
+  }
+  let result = 0;
+  array.forEach(element => {
+    result += sum([element]);
+  });
+  return result;
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
