38725c8bb07042426a921be322a4dd148a805a99
Glenmore Vinoya <glenmore@pinoyak.com>
2021-01-03 05:54:50 +0000

    completed sum nested array of integers

--- src/recursion.js
+++ src/recursion.js
@@ -28,6 +28,18 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0) {
+    return 0;
+  }
+  let total = 0;
+  array.forEach(element => {
+    if (Array.isArray(element)) {
+      total += arraySum(element);
+    } else {
+      total += element;
+    }
+  });
+  return total;
 };
 
 // 4. Check if a number is even.
